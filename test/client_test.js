712d9b5b21275149d43347293c469dc7542b2339
pdfcrowd dev <info+github@pdfcrowd.com>
2012-07-05 01:27:50 +0200

    Added a test for .zip files.

--- test/client_test.js
+++ test/client_test.js
@@ -5,18 +5,19 @@ credentials = require('./config').Credentials;
 myPdfcrowd = new pdf.Pdfcrowd(credentials.username,
                               credentials.apikey);
 
+var apiOptions =     {
+    width: "11in",
+    height: "8.5in",
+    vmargin: ".4in",
+    footer_html: '<div style=text-align:center;font-size:smaller;color:maroon;">\
+                              Page %p out of %n\
+                          </div>'
+}
+
+myPdfcrowd.convertFile("sample.html.zip", pdf.saveToFile("zfile.pdf"), apiOptions);
 myPdfcrowd.convertHtml("raw code", pdf.saveToFile("html.pdf"));
 myPdfcrowd.convertURI("http://example.com", pdf.saveToFile("url.pdf"));
 myPdfcrowd.convertFile("sample.html", pdf.saveToFile("file.pdf"));
-myPdfcrowd.convertHtml(
-    'footer example', 
-    pdf.saveToFile("footer.pdf"),
-    {
-        width: "11in",
-        height: "8.5in",
-        vmargin: ".4in",
-        footer_html: '<div style=text-align:center;font-size:smaller;color:maroon;">\
-                              Page %p out of %n\
-                          </div>'
-    });
+myPdfcrowd.convertHtml('footer example', pdf.saveToFile("footer.pdf"), apiOptions);
+
 
